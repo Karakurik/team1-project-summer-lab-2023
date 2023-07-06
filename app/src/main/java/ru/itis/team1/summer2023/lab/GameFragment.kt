a45a536e71453d668bbd76d5e014a47af5dbba04
liiceberg <2aysylu2003@gmail.com>
2023-07-06 23:23:18 +0300

    addTextChangedListener work

--- app/src/main/java/ru/itis/team1/summer2023/lab/GameFragment.kt
+++ app/src/main/java/ru/itis/team1/summer2023/lab/GameFragment.kt
@@ -1,5 +1,6 @@
 package ru.itis.team1.summer2023.lab
 
+import android.content.res.ColorStateList
 import android.graphics.Color
 import android.os.Bundle
 import android.text.Editable
@@ -13,6 +14,7 @@ import android.view.ViewGroup
 import android.widget.EditText
 import com.google.android.material.textfield.TextInputEditText
 import ru.itis.team1.summer2023.lab.databinding.FragmentGameBinding
+import ru.itis.team1.summer2023.lab.databinding.WordBinding
 
 
 class GameFragment : Fragment(R.layout.fragment_game) {
@@ -24,38 +26,91 @@ class GameFragment : Fragment(R.layout.fragment_game) {
         binding = FragmentGameBinding.bind(view)
 
         binding?.run {
-
-            val list: List<TextInputEditText> = listOf(
-                layoutWord1.letter1.etLetter,
-                layoutWord1.letter2.etLetter,
-                layoutWord1.letter3.etLetter,
-                layoutWord1.letter4.etLetter,
-                layoutWord1.letter5.etLetter
+            val wordsList: List<WordBinding> = listOf(
+                layoutWord1,
+                layoutWord2,
+                layoutWord3,
+                layoutWord4,
+                layoutWord5,
+                layoutWord6
             )
 
-            layoutWord1.letter1.etLetter.addTextChangedListener(object : TextWatcher {
-                    override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                    }
+            wordsList.forEach { word ->
+                val lettersList: List<TextInputEditText> = listOf(
+                    word.letter1.etLetter,
+                    word.letter2.etLetter,
+                    word.letter3.etLetter,
+                    word.letter4.etLetter,
+                    word.letter5.etLetter,
+                    word.letter6.etLetter
+                )
+                var flag = false
+//                TODO ломается когда есть скрытые элементы
+                for ((i, letter) in lettersList.withIndex()) {
+                    if (letter.visibility == View.VISIBLE
 
-                    override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                        if (layoutWord1.letter1.etLetter.text.toString().isNotEmpty()) {
-                            layoutWord1.letter2.etLetter.requestFocus()
+                    ) {
+                        if ((i + 1) < lettersList.size
+                            && lettersList[i + 1].visibility == View.VISIBLE
+                        ) {
+                            letter.addTextChangedListener(object : TextWatcher {
+                                override fun beforeTextChanged(
+                                    p0: CharSequence?,
+                                    p1: Int,
+                                    p2: Int,
+                                    p3: Int
+                                ) {
+                                }
+
+                                override fun onTextChanged(
+                                    p0: CharSequence?,
+                                    p1: Int,
+                                    p2: Int,
+                                    p3: Int
+                                ) {
+                                    if (letter.text.toString().isNotEmpty()) {
+                                        letter.focusSearch(View.FOCUS_RIGHT).requestFocus()
+                                    }
+                                }
+
+                                override fun afterTextChanged(p0: Editable?) {}
+                            })
+                        } else while (!flag) {
+                            if (letter.text.toString().isNotEmpty()) flag = true
                         }
                     }
+                }
 
-                    override fun afterTextChanged(p0: Editable?) {
-//                        layoutWord1.letter1.etLetter.inputType = InputType.TYPE_NULL
-//                               TODO: set colors
-                    }
-                })
+                val str = "HELLO"
 
+                if (verifyWord() && flag) {
+                    convertWord(lettersList, str)
+                } else {
 
+                }
+            }
         }
 
     }
 
-    private fun getNext() {
+    private fun convertWord(lettersList: List<TextInputEditText>, answer: String) {
+        for ((i, letter) in lettersList.withIndex()) {
+            letter.inputType = InputType.TYPE_NULL
+            val value: String = letter.text.toString()
+            if (answer.contains(value)) {
+                if (answer.indexOf(value) == i) {
+                    letter.setTextColor(resources.getColor(R.color.bull))
+                } else {
+                    letter.setTextColor(resources.getColor(R.color.cow))
+                }
+            } else {
+                letter.setTextColor(resources.getColor(R.color.miss))
+            }
+        }
+    }
 
+    private fun verifyWord(): Boolean {
+        return true
     }
 
     override fun onDestroyView() {
--- app/src/main/java/ru/itis/team1/summer2023/lab/MainActivity.kt
+++ app/src/main/java/ru/itis/team1/summer2023/lab/MainActivity.kt
@@ -1,5 +1,6 @@
 package ru.itis.team1.summer2023.lab
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,6 +8,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_game)
+        setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().replace(R.id.main, GameFragment()).commit()
     }
 }
