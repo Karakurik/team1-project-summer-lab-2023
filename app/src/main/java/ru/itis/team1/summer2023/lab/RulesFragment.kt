6079ff241187148b3876be62111280f412cb5974
Forsythe24 <miron.solopov@yandex.ru>
2023-07-07 19:21:02 +0900

    individualized oldman's messages

--- app/src/main/java/ru/itis/team1/summer2023/lab/RulesFragment.kt
+++ app/src/main/java/ru/itis/team1/summer2023/lab/RulesFragment.kt
@@ -2,6 +2,7 @@ package ru.itis.team1.summer2023.lab
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.google.android.material.snackbar.Snackbar
 import ru.itis.team1.summer2023.lab.databinding.FragmentRulesBinding
@@ -15,23 +16,28 @@ class RulesFragment: Fragment(R.layout.fragment_rules) {
         binding = FragmentRulesBinding.bind(view)
         binding?.apply {
             btnRespond.setOnClickListener{
-
-                if (checkIfValid(etM1Player.text.toString())) {
+                val name = etM1Player.text.toString()
+                if (clickCount == 1) {
+                    show(ivPlayer2)
+                    show(tvM2Player)
+                    show(ivOldman3)
+                    show(tvM3Oldman)
+                    clickCount++
+                } else if (clickCount == 2) {
+                    show(ivPlayer3)
+                    show(tvM3Player)
+                    btnRespond.text = resources.getString(R.string.got_it_message)
+                    clickCount++
+                    // from now on the button must bring Player back to the Main screen
+                }
+                if (checkIfValidName(name) && clickCount == 0) {
+                    etM1Player.isFocusable = false
                     show(ivOldman2)
                     show(tvM2Oldman)
-                    if (clickCount == 1) {
-                        show(ivPlayer2)
-                        show(tvM2Player)
-                        show(ivOldman3)
-                        show(tvM3Oldman)
-                    } else if (clickCount == 2) {
-                        show(ivPlayer3)
-                        show(tvM3Player)
-                        btnRespond.text = resources.getString(R.string.got_it_message)
-                        // from now on the button must bring Player back to the Main screen
-                    }
+                    setIndividualMessage(tvM2Oldman, name)
+                    setIndividualMessage(tvM3Oldman, name)
                     clickCount++
-                } else {
+                } else if (clickCount == 0) {
                     showSnackbar(view)
                 }
             }
@@ -44,11 +50,14 @@ class RulesFragment: Fragment(R.layout.fragment_rules) {
         snackbar.setDuration(8000).setTextMaxLines(4).show()
     }
 
+    private fun setIndividualMessage(view: TextView, username: String) {
+        view.text = view.text.toString().replace("Name", username)
+    }
     private fun show(view: View) {
         view.visibility = View.VISIBLE
     }
 
-    private fun checkIfValid(name: String): Boolean {
+    private fun checkIfValidName(name: String): Boolean {
         val regex = "[A-Za-z0-9]+".toRegex()
         return regex.matches(name)
     }
