b7c656fcf2ad537430b63e4b806f17b261cd1e82
Forsythe24 <miron.solopov@yandex.ru>
2023-07-07 18:53:29 +0900

    added snackbar and fixed username validation

--- app/src/main/java/ru/itis/team1/summer2023/lab/RulesFragment.kt
+++ app/src/main/java/ru/itis/team1/summer2023/lab/RulesFragment.kt
@@ -3,6 +3,7 @@ package ru.itis.team1.summer2023.lab
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
 import ru.itis.team1.summer2023.lab.databinding.FragmentRulesBinding
 
 
@@ -27,11 +28,20 @@ class RulesFragment: Fragment(R.layout.fragment_rules) {
                         show(ivPlayer3)
                         show(tvM3Player)
                         btnRespond.text = resources.getString(R.string.got_it_message)
+                        // from now on the button must bring Player back to the Main screen
                     }
                     clickCount++
+                } else {
+                    showSnackbar(view)
                 }
             }
         }
+    }
+
+    private fun showSnackbar(view: View) {
+        val snackbar = Snackbar.make(view, "The username has a min length of 1 and a max length of 25 and" +
+                " can only use letters of Latin alphabet and digits", Snackbar.LENGTH_LONG)
+        snackbar.setDuration(8000).setTextMaxLines(4).show()
     }
 
     private fun show(view: View) {
@@ -39,7 +49,7 @@ class RulesFragment: Fragment(R.layout.fragment_rules) {
     }
 
     private fun checkIfValid(name: String): Boolean {
-        val regex = "[A-Za-z]+".toRegex()
+        val regex = "[A-Za-z0-9]+".toRegex()
         return regex.matches(name)
     }
 
